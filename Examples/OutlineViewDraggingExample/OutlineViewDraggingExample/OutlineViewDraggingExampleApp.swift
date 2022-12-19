f4ee92e958836d35269480c4724f2b3b76179cc7
RCCoop <guthookhikes@gmail.com>
2022-12-19 12:37:41 -0500

    Renamed drag&drop sample in order to re-add original

--- Examples/OutlineViewDraggingExample/OutlineViewDraggingExample/OutlineViewDraggingExampleApp.swift
+++ Examples/OutlineViewDraggingExample/OutlineViewDraggingExample/OutlineViewDraggingExampleApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct OutlineViewExampleApp: App {
+struct OutlineViewDraggingExampleApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
